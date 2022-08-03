951d4b7ccd891026b113e9ffe3326ef47eda6b86
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-08-03 10:31:39 -0700

    Fixing connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = process.env.MOBGODB_URI || 'mongodb://localhost:27017/usersDB';
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/usersDB';
 
 connectionString(connectionString, {
     userNewUrlParser: true,
