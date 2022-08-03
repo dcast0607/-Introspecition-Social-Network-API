59fad769771ca591ff14e716969211b62d9d2aa6
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-08-02 20:00:46 -0700

    Pushing model for users

--- models/User.js
+++ models/User.js
@@ -1,2 +1,29 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
+const thoughtSchema = require('./Thought');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [thoughtSchema],
+        friends: [this]
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+model.exports = User;
